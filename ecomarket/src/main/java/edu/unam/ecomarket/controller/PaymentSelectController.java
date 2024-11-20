c7397941ae84d8318bd95186f02abd5aab7abbdf
EzequielHLC <hanzolovescoffee@gmail.com>
2024-11-20 19:24:59 -0300

    Nuevo Commit: Inicio de configuración de persistencia (Postgres y Clase Usuario)

--- ecomarket/src/main/java/edu/unam/ecomarket/controller/PaymentSelectController.java
+++ ecomarket/src/main/java/edu/unam/ecomarket/controller/PaymentSelectController.java
@@ -4,14 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class paymentController {
-
-    public paymentController() {
+public class PaymentSelectController {
+    
+    public PaymentSelectController() {
 
     }
 
     @RequestMapping("/payment")
-    public String pagar() {
+    public String metodoPago() {
         return "payment";
     }
 }
