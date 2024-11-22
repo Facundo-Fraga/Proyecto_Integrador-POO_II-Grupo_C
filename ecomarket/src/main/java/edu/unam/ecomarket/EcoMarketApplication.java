892bcb0dbf7bdc05cfe7c97ad4c691608ae73720
Iñaki <agustinerreguerena060504@gmail.com>
2024-11-22 11:07:57 -0300

    exclude para poder ejecutar el proyecto.

--- ecomarket/src/main/java/edu/unam/ecomarket/EcoMarketApplication.java
+++ ecomarket/src/main/java/edu/unam/ecomarket/EcoMarketApplication.java
@@ -2,12 +2,12 @@ package edu.unam.ecomarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})  
 public class EcoMarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcoMarketApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EcoMarketApplication.class, args);
+    }
 }
